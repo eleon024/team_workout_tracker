efa213841ddadc5395d059003738c33036df8e06
Carter <carterbrostron@gmail.com>
2025-02-14 22:14:37 -0500

    added nav bar, social, and updated calendar. pre logging implementation

--- workout/src/App.js
+++ workout/src/App.js
@@ -3,13 +3,16 @@ import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import Logging from './Logging';
 import Calendar from './Calendar';
+import Social from './Social';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand as={Link} to="/">My New Project</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Team Workout Tracker</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
@@ -18,9 +21,15 @@ function App() {
                 <NavDropdown.Item as={Link} to="/logging">
                   Logging
                 </NavDropdown.Item>
+                <br></br>
                 <NavDropdown.Item as={Link} to="/calendar">
                   Calendar
                 </NavDropdown.Item>
+                <br></br>
+                <NavDropdown.Item as={Link} to="/social">
+                  Team Feed
+                </NavDropdown.Item>
+                <br></br>
               </NavDropdown>
               {/* Additional Nav Links if needed */}
               <Nav.Link as={Link} to="/about">About</Nav.Link>
@@ -33,7 +42,8 @@ function App() {
             <Route path="/logging" element={<Logging />} />
             {/* Define additional routes here */}
             <Route path="/calendar" element={<Calendar/> }/>
-            <Route path="/about" element={<div>About Page</div>} />
+            <Route path="/social" element={<Social/> }/>
+            <Route path="/about" element={<div>A team workout tracker by Edgar, Carter, Ben, and MDP.</div>} />
             {/* Default route */}
             <Route path="/" element={<div>Welcome to My New Project!</div>} />
           </Routes>
--- workout/src/Calendar.js
+++ workout/src/Calendar.js
@@ -6,8 +6,7 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 function Calendar() {
   return (
     <div>
-      <h2>Calendar Page</h2>
-      <p>This is where the Calendar functionality will be implemented.</p>
+      <h2>Team Calendar</h2>
 
       <FullCalendar
       plugins={[ dayGridPlugin ]}
