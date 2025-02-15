c8cd9ac981f24a2f1c51b199da69d105992a3904
Edgar Leon <edgarleon@edgars-air.haverford.edu>
2025-02-14 20:49:59 -0500

    Stuff

--- workout/src/App.js
+++ workout/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import Logging from './Logging';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand as={Link} to="/">My New Project</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              {/* General Dropdown Menu */}
+              <NavDropdown title="Menu" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/logging">
+                  Logging
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/other">
+                  Other Option
+                </NavDropdown.Item>
+              </NavDropdown>
+              {/* Additional Nav Links if needed */}
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Container className="mt-4">
+          <Routes>
+            {/* Route to Logging.js component */}
+            <Route path="/logging" element={<Logging />} />
+            {/* Define additional routes here */}
+            <Route path="/other" element={<div>Other Option Page</div>} />
+            <Route path="/about" element={<div>About Page</div>} />
+            {/* Default route */}
+            <Route path="/" element={<div>Welcome to My New Project!</div>} />
+          </Routes>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
---
+++ workout/src/Logging.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Logging() {
+  return (
+    <div>
+      <h2>Logging Page</h2>
+      <p>This is where the logging functionality will be implemented.</p>
+    </div>
+  );
+}
+
+export default Logging;
